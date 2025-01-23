5d61a333fa58f678904c877c0b41ac37a0cf53c3
Derrick-Nuby <iradukundaderrick7@gmail.com>
2025-01-23 18:05:36 +0200

    removed swagger

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,10 +1,11 @@
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
-import swaggerjson from "@/data/swagger.json";
+// import SwaggerUI from "swagger-ui-react";
+// import "swagger-ui-react/swagger-ui.css";
+// import swaggerjson from "@/data/swagger.json";
 const page = () => {
   return (
     <div>
-      <SwaggerUI spec={swaggerjson} />
+      {/* <SwaggerUI spec={swaggerjson} /> */}
+      <h1>hello world</h1>
     </div>
   );
 };
